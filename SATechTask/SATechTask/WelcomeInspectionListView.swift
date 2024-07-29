37cb0901ef71526faac7c70cae9107f2dbccd6f9
Swapnil <swapnil@Swapnils-MacBook-Air.local>
2024-07-29 15:20:10 +0530

    Added changes

--- SATechTask/SATechTask/WelcomeInspectionListView.swift
+++ SATechTask/SATechTask/WelcomeInspectionListView.swift
@@ -6,65 +6,58 @@
 //
 
 import SwiftUI
-
 struct WelcomeInspectionListView: View {
     @StateObject private var inspectionService = InspectionNetworkService()
-    @State var inspections = [Inspection]()
+    @State private var inspections = [Inspection]()
     
     var body: some View {
         NavigationView {
-            GeometryReader { geometry in
-                VStack {
-                    HeaderView()
-                    
-                    ScrollView {
-                        VStack(alignment: .leading, spacing: 16) {
-                            SectionHeaderView(title: Constants.Questions.drugsSection)
-                            
-                            if inspections.count > 0 {
-                                ForEach(inspections) { inspection in
-                                    ForEach(inspection.survey.categories) { category in
-//                                        SectionHeaderView(title: "Drugs")
-                                        
-//                                        ForEach(category.questions) { question in
-//                                            QuestionView(question: question.text, options: question.options)
-//                                                .font(.system(size: geometry.size.width < 600 ? 14 : 18))
-//                                        }
-                                    }
-                                }
-                            } else {
+            VStack {
+                HeaderView()
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 16) {
+                        SectionHeaderView(title: Constants.Questions.drugsSection)
+                        
+                        if !inspections.isEmpty {
+                            // Render dynamically based on inspections data
+                            ForEach(inspections, id: \.id) { inspection in
+                                // Use the inspection data here
                                 QuestionView(question: Constants.Questions.isDrugsTrolleyLocked, options: [Constants.Questions.Options.yes, Constants.Questions.Options.no, Constants.Questions.Options.na])
-                                    .font(.system(size: geometry.size.width < 600 ? 14 : 18))
-                                
                                 QuestionView(question: Constants.Questions.howOftenIsFloorCleaned, options: [Constants.Questions.Options.everyday, Constants.Questions.Options.everyTwoDays, Constants.Questions.Options.everyWeek])
-                                    .font(.system(size: geometry.size.width < 600 ? 14 : 18))
-                                
-                                SectionHeaderView(title: Constants.Questions.overallImpressionsSection)
-                                
-                                QuestionView(question: "How many staff members are present in the ward?", options: ["1-2", "3-6", "6+", "N/A"])
-                                    .font(.system(size: geometry.size.width < 600 ? 14 : 18))
                             }
+                        } else {
+                            Text("No inspections available.")
+                                .foregroundColor(.gray)
                         }
-                        .padding()
+
+                        SectionHeaderView(title: Constants.Questions.overallImpressionsSection)
+                        
+                        // Static question view
+                        QuestionView(question: "How many staff members are present in the ward?", options: ["1-2", "3-6", "6+", "N/A"])
                     }
-                    
-                    ProgressView()
-                        .onAppear {
-                            inspectionService.fetchRandomInspection { result in
-                                switch result {
-                                case .success(let inspection):
-                                    self.inspections.append(inspection)
-                                case .failure(let error):
-                                    
-                                    print(error.localizedDescription)
-                                }
-                            }
-                        }
-                    
-                    SubmitButtonView()
+                    .padding()
                 }
-                .navigationBarHidden(true)
-                .navigationBarBackButtonHidden()
+                .onAppear {
+                    loadInspectionData()
+                }
+                
+                SubmitButtonView()
+            }
+            .navigationBarHidden(true)
+            .navigationBarBackButtonHidden()
+        }
+    }
+    
+    private func loadInspectionData() {
+        inspectionService.fetchRandomInspection { result in
+            switch result {
+            case .success(let inspection):
+                DispatchQueue.main.async {
+                    self.inspections.append(inspection)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
             }
         }
     }
